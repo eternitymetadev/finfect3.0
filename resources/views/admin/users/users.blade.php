0a86e65ba37812b44ff932852c9bca3c350eeed0
vikaschandel <vikasssingh438@gmail.com>
2023-12-22 17:48:02 +0530

    roles view

--- resources/views/admin/users/users.blade.php
+++ resources/views/admin/users/users.blade.php
@@ -191,7 +191,7 @@
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
-
+            
             <div class="d-flex align-items-center flex-wrap">
                 <div class="form-row">
                     <div class="form-group col-md-6">
